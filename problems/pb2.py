fceba978bdfef8998cb588df4809092d241d065e
shaneprice87 <shane.price@fiserv.com>
2024-08-13 14:53:09 -0500

    Update pb2.py

--- problems/pb2.py
+++ problems/pb2.py
@@ -1,4 +1,12 @@
 # declare and fill out function here
-
+def max_values(l):
+  max = l[0]
+  for i in l:
+    if i > max:
+     max = i
+    for j in l:
+      if j > max & j != max:
+        max2 = j
+  return l.index(max), l.index(max2)
 # test case
 print(max_values2([-5, -2, -1, -11])) # -> [1, 2]
